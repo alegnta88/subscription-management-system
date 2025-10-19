66c0b2f3f5e22955233b4ff0a3facd738862bb10
root <root@alegnta.digaf.local>
2025-10-19 22:18:08 +0300

    middleware-changes

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
   res.send("Hello, Subscription Tracker API!");
