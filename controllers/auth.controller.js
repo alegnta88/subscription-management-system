a3f2af740b03e4736e50a442695b73f06c7d5ace
root <root@alegnta.digaf.local>
2025-10-20 09:53:56 +0300

    new-uesr-registration

---
+++ controllers/auth.controller.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
+
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email }).session(session);
+    if (existingUser) {
+      const error = new Error('Email is already in use');
+      error.status = 400;
+      throw error;
+    }
+
+    // hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create([{ name, email, password: hashedPassword }], { session });
+
+    const token = jwt.sign({ userId: newUser[0]._id }, process.env.JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: 'User registered successfully',
+      data: {
+        token,
+        user: newUser[0],
+
+    }});
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  res.send('Signin route');
+};
+
+export const signOut = async (req, res, next) => {
+  res.send('Signout route');
+};
