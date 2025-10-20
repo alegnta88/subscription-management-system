edf910335ec552e7638556a764a6b869671d0e7d
root <root@alegnta.digaf.local>
2025-10-20 11:34:21 +0300

    controller/auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -49,7 +49,38 @@ export const signUp = async (req, res, next) => {
 };
 
 export const signIn = async (req, res, next) => {
-  res.send('Signin route');
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) { 
+      const error = new Error('User not found');
+      error.status = 404;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      const error = new Error('Invalid password');
+      error.status = 401;
+      throw error;
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+    
+    res.status(200).json({
+      success: true,
+      message: 'User signed in successfully',
+      data: {
+        token,
+        user,
+      },
+    });
+
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signOut = async (req, res, next) => {
